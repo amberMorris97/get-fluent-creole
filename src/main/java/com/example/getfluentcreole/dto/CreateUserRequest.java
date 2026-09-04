afcf9f334ca3d1328e757dbc7f179369c3eacc0c
Amber Morris <ambermorris1997@gmail.com>
2026-09-04 17:33:09 -0400

    create user service and controller

---
+++ src/main/java/com/example/getfluentcreole/dto/CreateUserRequest.java
@@ -0,0 +1,28 @@
+package com.example.getfluentcreole.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class CreateUserRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email address is required")
+    private String emailAddress;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
