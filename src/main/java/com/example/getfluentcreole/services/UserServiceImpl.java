a72a3f1ae7ead955d55d48cf8d78e302f6e2901d
ambermorris97 <ambermorris1997@gmail.com>
2026-09-06 14:21:02 -0400

    create app config and add model mapper bean

--- src/main/java/com/example/getfluentcreole/services/UserServiceImpl.java
+++ src/main/java/com/example/getfluentcreole/services/UserServiceImpl.java
@@ -1,8 +1,18 @@
 package com.example.getfluentcreole.services;
 
+import com.example.getfluentcreole.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl {
-    
+    private final ModelMapper modelMapper;
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(ModelMapper modelMapper, UserRepository userRepository) {
+        this.modelMapper = modelMapper;
+        this.userRepository = userRepository;
+    }
+
+
 }
