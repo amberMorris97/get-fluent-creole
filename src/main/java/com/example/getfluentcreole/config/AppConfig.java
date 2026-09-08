e24935e701afa64c7aee86d6d54537cd6194b4bb
Amber Morris <ambermorris1997@gmail.com>
2026-09-08 13:24:58 -0400

    create bean for password encoder

--- src/main/java/com/example/getfluentcreole/config/AppConfig.java
+++ src/main/java/com/example/getfluentcreole/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.example.getfluentcreole.config;
 
+import org.jspecify.annotations.Nullable;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,20 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public @Nullable String encode(@Nullable CharSequence rawPassword) {
+                return "";
+            }
+
+            @Override
+            public boolean matches(@Nullable CharSequence rawPassword, @Nullable String encodedPassword) {
+                return false;
+            }
+        };
     }
 }
