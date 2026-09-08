2cb0b92f830684842a0af1bb03ff5be657c7e91b
Amber Morris <ambermorris1997@gmail.com>
2026-09-08 11:27:36 -0400

    add getters and setters for user response dto

--- src/main/java/com/example/getfluentcreole/dto/response/UserResponseDTO.java
+++ src/main/java/com/example/getfluentcreole/dto/response/UserResponseDTO.java
@@ -12,4 +12,28 @@ public class UserResponseDTO {
         this.lastName = lastName;
         this.emailAddress = emailAddress;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
